cc92b21f4e31080b7ce0d22103adc9807ddf77f0
Brian Manley <brian.manley91@gmail.com>
2022-12-04 17:43:08 -0500

    finished day 2

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -4,8 +4,21 @@ RSpec.describe RockPaperScissors do
     it 'returns the total score if the strategy guide is followed' do
         expected = 15
 
-        actual = RockPaperScissors.calc_total_score('data/day_2/test_input.txt')
+        actual = RockPaperScissors.new.calc_total_score('data/day_2/test_input.txt')
     
         expect(actual).to eq(expected)
+    end
+
+    it 'returns the total score if the strategy guide with corrected instructions is followed' do
+        expected = 12
+
+        actual = RockPaperScissorsCorrected.new.calc_total_score('data/day_2/test_input.txt')
+    
+        expect(actual).to eq(expected)
+    end
+
+    it 'Day 2 solutions' do
+        puts "Day 2 Solution 1 #{RockPaperScissors.new.calc_total_score('data/day_2/input.txt')}"
+        puts "Day 2 Solution 2 #{RockPaperScissorsCorrected.new.calc_total_score('data/day_2/input.txt')}"
     end
 end
