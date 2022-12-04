3c2cffa4fa36d7647690305032382d6a48b820a4
Brian Manley <brian.manley91@gmail.com>
2022-12-04 13:36:47 -0500

    Day 4 part 2

--- lib/camp_cleanup.rb
+++ lib/camp_cleanup.rb
@@ -1,30 +1,47 @@
 class CampCleanup
-    def self.find_total_overlaps(input_file)
-        total_overlap_count = 0
+    def self.find_full_overlaps(input_file)
+        full_overlap_count = 0
         File.open(input_file) do |file|
             file.each_line do |line|
-                # Parse each line into pairs
                 ranges = parse_range_line(line)
-                # puts ranges.to_s
+
                 if does_range_contain(ranges[0], ranges[1]) || does_range_contain(ranges[1], ranges[0])
-                    total_overlap_count+=1
+                    full_overlap_count+=1
                 end
             end
         end
-        return total_overlap_count
+        return full_overlap_count
     end
 
-    # Split format 1-2,3-4 into two ranges
+    def self.find_any_overlaps(input_file)
+        overlap_count = 0
+        File.open(input_file) do |file|
+            file.each_line do |line|
+                ranges = parse_range_line(line)
+                
+                if do_ranges_overlap(ranges[0], ranges[1])
+                    overlap_count+=1
+                end
+            end
+        end
+        return overlap_count
+    end
+
+    # Split format 1-2,3-4 into two range structs
     def self.parse_range_line(file_line)
         range_strings = file_line.split(',')
-        range_strings.map { |range_string|
+        return range_strings.map { |range_string|
             numbers = range_string.split('-')
             CleanupRange.new(numbers[0].to_i, numbers[1].to_i)
         }
     end
 
     def self.does_range_contain(range1, range2)
-        range1.start <= range2.start && range1.finish >= range2.finish
+        return range1.start <= range2.start && range1.finish >= range2.finish
+    end
+
+    def self.do_ranges_overlap(range1, range2)
+        return range2.start <= range1.finish && range1.start <= range2.finish
     end
 end
 
